73b38667b404561b6992f0a60f07168a26c7412f
Joe <joe.holmes87@gmail.com>
2021-01-10 18:08:26 -0500

    configured sanity

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -18,27 +18,14 @@ export default {
       },
     },
     {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    },
-    {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
       name: 'categories',
       title: 'Categories',
       type: 'array',
       of: [{type: 'reference', to: {type: 'category'}}],
     },
+    //be sure to change the name to "date" so it's easily accessed in Hugo
     {
-      name: 'publishedAt',
+      name: 'date',
       title: 'Published at',
       type: 'datetime',
     },
@@ -48,18 +35,4 @@ export default {
       type: 'blockContent',
     },
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
-      })
-    },
-  },
 }
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,7 +8,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
-import author from './author'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,7 +19,6 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     post,
-    author,
     category,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
